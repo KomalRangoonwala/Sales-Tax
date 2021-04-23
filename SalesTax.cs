b95315bf16f36813ec62ca72f73a3a49057c9125
KomalR <30585552+KomalRangoonwala@users.noreply.github.com>
2021-04-23 12:25:46 +0530

    Update SalesTax.cs
    Refactored the code and incorporated OOPs concepts. Changed the nomenclature according to .NET standards for code readability.

--- SalesTax.cs
+++ SalesTax.cs
@@ -1,90 +1,244 @@
 using System;
-using System.Linq;
 using System.Collections.Generic;
 
 // ---------------------------------------------------
 // Author: Komal Rangoonwala for Makkajai Dev Challenge
-// Date: 17 Apr 2021
+// Date: 17 Apr 2021, 21 Apr 2021, 22 Apr 2021, 23 Apr 2021
 // ---------------------------------------------------
 
 // A structure to hold and manage the data
 public class Program
 {
-	public class Order
+	public enum ProductCategory
 	{
-		List<Product> objList = new List<Product>();
-		public Order(List<Product> objList_)
-		{
-			this.objList = objList_;
-		}
+		Books = 1,
+		Food = 2,
+		MedicalProducts = 3,
+		Other = 4,
+	}
+	
+	public enum INPUT_TYPE
+	{
+		SYSTEM = 1,
+		USER = 2,
 	}
 	
 	public class Product
 	{
-		public string Name = string.Empty;
-		public double Price = 0;
-		public int Quantity = 0;
+		#region Properties
+		string _name = string.Empty;
+		double _price = 0;
+		bool _isImported = false;
+		ProductCategory _category = ProductCategory.Other;
+		int _quantity = 0;
+		double _totalPrice = 0;
 		
-		public Product(string Name_, double Price_, int Quantity_)
+		public string Name
 		{
-			this.Name = Name_;
-			this.Price = Price_;
-			this.Quantity = Quantity_;
+			get
+			{
+				return this._name;
+			}
 		}
+		
+		public double Price
+		{
+			get
+			{
+				return this._price;
+			}
+		}
+		
+		public bool IsImported
+		{
+			get
+			{
+				return this._isImported;
+			}
+		}
+		
+		public ProductCategory Category
+		{
+			get
+			{
+				return this._category;
+			}
+		}
+		
+		public int Quantity
+		{
+			get
+			{
+				return this._quantity;
+			}
+		}
+		
+		public double TotalPrice
+		{
+			get
+			{
+				return this._totalPrice;
+			}
+		}
+		#endregion
+
+		#region Constructors
+		public Product(string Name, double Price, bool IsImported, ProductCategory Category, int Quantity)
+		{
+			this._name = Name;
+			this._price = Price;
+			this._isImported = IsImported;
+			this._category = Category;
+			this._quantity = Quantity;
+			this._totalPrice = Price * Quantity;
+		}
+		#endregion
+	}
+	
+	public interface ITax
+	{
+		Product GetProduct();
+		double Calculate();
+	}
+	
+	public class BaseTax : ITax
+	{
+		Product _product;
+		public BaseTax(Product product)
+		{
+			this._product = product;
+		}
+		
+		public Product GetProduct()
+		{
+			return this._product;
+		}
+
+		public double Calculate()
+		{
+			return 0;
+		}
+	}
+	
+	public class RegularTax : ITax
+	{
+		Product _product;
+		public RegularTax(Product product)
+		{
+			this._product = product;
+		}
+		
+		public Product GetProduct()
+		{
+			return this._product;
+		}
+		
+		public double Calculate()
+		{
+			return RoundUpTo(this._product.TotalPrice * 0.1);
+		}
+	}
+	
+	public class ImportTax : ITax
+	{
+		Product _product;
+		public ImportTax(Product product)
+		{
+			this._product = product;
+		}
+		
+		public Product GetProduct()
+		{
+			return this._product;
+		}
+
+		public double Calculate()
+		{
+			return RoundUpTo(this._product.TotalPrice * 0.05);
+		}
+	}
+	
+	public class Invoice
+	{
+		#region Properties
+		List<Product> _invoice = new List<Product>();
+		double _totalTax = 0;
+		double _totalAmount = 0;
+		#endregion
+
+		#region Constructors
+		public Invoice(List<Product> invoice, double totalTax, double totalAmount)
+		{
+			this._invoice = invoice;
+			this._totalTax = totalTax;
+			this._totalAmount = totalAmount;
+		}
+		#endregion
+
+		#region Methods
+		public void Print()
+		{
+			Console.WriteLine("\nYour invoice:\n");
+			foreach(Product objProduct in this._invoice)
+				Console.WriteLine(objProduct.Quantity + " " + objProduct.Name + " at " + objProduct.Price);
+
+			Console.WriteLine("Sales Taxes: " + this._totalTax.ToString("F2") + "\nTotal: " + this._totalAmount.ToString());
+		}
+		#endregion
 	}
 	
 	#region Input methods
 	// Method to test some fixed inputs as given in the question
 	public static List<Product> GetFixedInput(int TestCase)
 	{
-		List<Product> lstProducts = new List<Product>();
+		List<Product> Products = new List<Product>();
 		switch(TestCase)
 		{
 			case 1:
-				Product objProduct1 = new Product("book", 12.49, 1);
-				lstProducts.Add(objProduct1);
+				Product objProduct1 = new Product("book", 12.49, false, ProductCategory.Books, 1);
+				Products.Add(objProduct1);
 				
-				Product objProduct2 = new Product("music CD", 14.99, 1);
-				lstProducts.Add(objProduct2);
+				Product objProduct2 = new Product("music CD", 14.99, false, ProductCategory.Other, 1);
+				Products.Add(objProduct2);
 				
-				Product objProduct3 = new Product("chocolate bar", 0.85, 1);
-				lstProducts.Add(objProduct3);
+				Product objProduct3 = new Product("chocolate bar", 0.85, false, ProductCategory.Food, 1);
+				Products.Add(objProduct3);
 				break;
 				
 			case 2:
-				Product objProduct4 = new Product("imported box of chocolates", 10.00, 1);
-				lstProducts.Add(objProduct4);
+				Product objProduct4 = new Product("imported box of chocolates", 10.00, true, ProductCategory.Food, 1);
+				Products.Add(objProduct4);
 				
-				Product objProduct5 = new Product("imported bottle of perfume", 47.50, 1);
-				lstProducts.Add(objProduct5);				
+				Product objProduct5 = new Product("imported bottle of perfume", 47.50, true, ProductCategory.Other, 1);
+				Products.Add(objProduct5);				
 				break;
 				
 			case 3:
-				Product objProduct6 = new Product("imported bottle of perfume", 27.99, 1);
-				lstProducts.Add(objProduct6);
+				Product objProduct6 = new Product("imported bottle of perfume", 27.99, true, ProductCategory.Other, 1);
+				Products.Add(objProduct6);
 				
-				Product objProduct7 = new Product("bottle of perfume", 18.99, 1);
-				lstProducts.Add(objProduct7);
+				Product objProduct7 = new Product("bottle of perfume", 18.99, false, ProductCategory.Other, 1);
+				Products.Add(objProduct7);
 				
-				Product objProduct8 = new Product("packet of headache pills", 9.75, 1);
-				lstProducts.Add(objProduct8);
+				Product objProduct8 = new Product("packet of headache pills", 9.75, false, ProductCategory.MedicalProducts, 1);
+				Products.Add(objProduct8);
 				
-				Product objProduct9 = new Product("box of imported chocolates", 11.25, 1);
-				lstProducts.Add(objProduct9);
+				Product objProduct9 = new Product("box of imported chocolates", 11.25, true, ProductCategory.Food, 1);
+				Products.Add(objProduct9);
 				break;
 		}
 		
-		return lstProducts;
+		return Products;
 	}
 	
 	// Method to test user input
 	public static List<Product> GetUserInput()
 	{
 		Console.WriteLine("Enter the number of products: ");
-		int intTotalProducts = Convert.ToInt32(Console.ReadLine());
+		int TotalProducts = Convert.ToInt32(Console.ReadLine());
 
-		List<Product> lstProducts = new List<Product>();
-		for(int i=1; i<=intTotalProducts; i++)
+		List<Product> Products = new List<Product>();
+		for ( int i = 1; i <= TotalProducts; i++ )
 		{
 			Console.WriteLine("#" + i.ToString() + " Enter product name: ");
 			string strName = Console.ReadLine();
@@ -92,14 +246,20 @@ public class Program
 			Console.WriteLine("#" + i.ToString() + " Enter product price: ");
 			double dblPrice = Convert.ToDouble(Console.ReadLine());
 			
+			Console.WriteLine("#" + i.ToString() + " Is product imported? [true or false] ");
+			bool blnIsImported = Convert.ToBoolean(Console.ReadLine());
+			
+			Console.WriteLine("#" + i.ToString() + " Enter product category [Books = 1, Food = 2, Medical products = 3, Other = 4]: ");
+			ProductCategory eCategory = (ProductCategory) Convert.ToInt32(Console.ReadLine());
+			
 			Console.WriteLine("#" + i.ToString() + " Enter product quantity: ");
 			int intQuantity = Convert.ToInt32(Console.ReadLine());
 			
-			Product objProduct = new Product(strName, dblPrice, intQuantity);
-			lstProducts.Add(objProduct);
+			Product objProduct = new Product(strName, dblPrice, blnIsImported, eCategory, intQuantity);
+			Products.Add(objProduct);
 		}
 		
-		return lstProducts;
+		return Products;
 	}
 	#endregion
 		
@@ -110,28 +270,29 @@ public class Program
 		try
 		{
 			Console.WriteLine("Do you want to test fixed input or user input? Enter 1 for fixed system input, 2 for user input:");
-			int intInputType = Convert.ToInt32(Console.ReadLine());
-			if(intInputType != 1 && intInputType != 2)
+			INPUT_TYPE InputType = (INPUT_TYPE) Convert.ToInt32(Console.ReadLine());
+			if(InputType != INPUT_TYPE.SYSTEM && InputType != INPUT_TYPE.USER)
 				throw new Exception("Invalid input! Please enter either 1 or 2.");
 
-			List<Product> lstProducts = null;
-			if(intInputType == 1)
+			List<Product> lstProducts = new List<Product>();
+			if(InputType == INPUT_TYPE.SYSTEM) // System input
 			{
 				Console.WriteLine("Which test case do you want to test? 1, 2, or 3?");
+				
 				int intTestCase = Convert.ToInt32(Console.ReadLine());
 				if(intTestCase < 1 || intTestCase > 3)
 					throw new Exception("Invalid test case! Please enter either 1, 2 or 3.");
 				
 				lstProducts = GetFixedInput(intTestCase);
 			}
-			else
+			else // User input
 				lstProducts = GetUserInput();
-
-			double dblTotalTax = 0;
-			double dblTotalAmount = 0;
-
-			List<Product> lstInvoice = ProcessOrders(lstProducts, ref dblTotalTax, ref dblTotalAmount);
-			PrintInvoice(lstInvoice, dblTotalTax, dblTotalAmount);
+			
+			if(lstProducts.Count <= 0)
+				throw new Exception("Product list is empty. System is unable to process the orders and generate the invoice.");
+			
+			Invoice objInvoice = ProcessOrders(lstProducts);
+			objInvoice.Print();
 		}
 		catch(Exception ex)
 		{
@@ -142,43 +303,44 @@ public class Program
 		
 	#region Core methods
 	// Method to process the actual data
-	public static List<Product> ProcessOrders(List<Product> lstProducts, ref double dblTotalTax, ref double dblTotalAmount)
+	public static Invoice ProcessOrders(List<Product> lstProducts)
 	{
 		List<Product> lstInvoice = new List<Product>();
+		double dblTotalTax = 0;
+		double dblTotalAmount = 0;
 		
-		foreach(Product objProduct in lstProducts)
+		foreach ( Product objProduct in lstProducts )
 		{
-			int intTotalTax = CalculateTax(objProduct.Name);
-			double dblFinalAmount = 0;
-			double dblTaxAmount = 0;
-			if(intTotalTax > 0)
+			double taxToApply = 0;
+			BaseTax bt = new BaseTax(objProduct);
+			double dblBaseTax = bt.Calculate();
+			taxToApply += dblBaseTax;
+
+			if ( objProduct.Category == ProductCategory.Other )
 			{
-				double dblSumAmount = objProduct.Price * objProduct.Quantity;
-				dblTaxAmount = RoundUpTo((dblSumAmount * intTotalTax) / 100);
-				dblTotalTax += dblTaxAmount;
-				dblFinalAmount = dblSumAmount + dblTaxAmount;
+				RegularTax rt = new RegularTax(objProduct);
+				double dblRegularTax = rt.Calculate();
+				taxToApply += dblRegularTax;
 			}
-			else
-				dblFinalAmount = objProduct.Price * objProduct.Quantity;
-
-			dblTotalAmount += dblFinalAmount;
-
-			Product objInvoiceProduct = new Product(objProduct.Name, dblFinalAmount, objProduct.Quantity);
+			
+			if ( objProduct.IsImported )
+			{
+				ImportTax it = new ImportTax(objProduct);
+				double dblImportTax = it.Calculate();
+				taxToApply += dblImportTax;
+			}
+			
+			dblTotalTax += taxToApply;
+			double dblPrice = objProduct.TotalPrice + taxToApply;
+			dblTotalAmount += dblPrice;
+				
+			Product objInvoiceProduct = new Product(objProduct.Name, dblPrice, objProduct.IsImported, objProduct.Category, objProduct.Quantity);
 			lstInvoice.Add(objInvoiceProduct);
 		}
 		
-		return lstInvoice;
+		return new Invoice(lstInvoice, dblTotalTax, dblTotalAmount);
 	}
 	
-	// Method to display/print the invoice generated and it is called by the Main method
-	public static void PrintInvoice(List<Product> lstInvoice, double dblTotalTax, double dblTotalAmount)
-	{
-		Console.WriteLine("\nYour invoice:\n");
-		foreach(Product objProduct in lstInvoice)
-			Console.WriteLine(objProduct.Quantity + " " + objProduct.Name + " at " + objProduct.Price);
-		
-		Console.WriteLine("Sales Taxes: " + dblTotalTax.ToString("F2") + "\nTotal: " + dblTotalAmount.ToString());
-	}
 	#endregion
 	
 	#region Utility methods
@@ -186,40 +348,6 @@ public class Program
 	{
 		// Rounds upto nearest 0.05;
 		return Math.Ceiling(ValueToRound * 20) / 20;
-	}
-	
-	public static bool IsImported(string ItemName)
-	{
-		if(ItemName.ToLower().Contains("imported"))
-			return true;
-		else
-			return false;
-	}
-	
-	public static bool IsExemptFromTax(string ItemName)
-	{
-		// More values can be added here depending on the data that we are expecting.
-		List<string> lstExemptItems = new List<string>(new string[] { "book", "food", "medicine", "pills", "chocolate", "chocolates", "novel" });
-		return lstExemptItems.Any(s=>ItemName.ToLower().Contains(s));
-	}
-	
-	// Method to calculate the tax of the given item
-	public static int CalculateTax(string ItemName)
-	{
-		int intRegularTax = 10;
-		int intImportTax = 5;
-		int intTotalTax = 0;
-		
-		bool blnShouldApplyImportTax = IsImported(ItemName);
-		bool blnShouldApplyRegularTax = IsExemptFromTax(ItemName) ? false : true;
-		
-		if(blnShouldApplyImportTax)
-			intTotalTax += intImportTax;
-		
-		if(blnShouldApplyRegularTax)
-			intTotalTax += intRegularTax;
-		
-		return intTotalTax;
 	}
 	#endregion
 }
